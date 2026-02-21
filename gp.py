d8769f9a5de279d91d8cb466c76ce31fe8f934ed
Nathan Wycoff <nathanbrwycoff@gmail.com>
2026-02-21 22:34:25 +0000

    Looks OK with fixed nug and self-exclusion

--- gp.py
+++ gp.py
@@ -10,9 +10,15 @@ from jax.numpy.linalg import norm
 from einops import rearrange
 import scipy
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 EPS = float(jnp.sqrt(jnp.finfo(float).eps))
 
+ADAPT_RHO = False
+#ADAPT_RHO = True
+
+SELF_ZERO = True
+#SELF_ZERO = False
 
 def squared_distance(
     x1: Float[Array, "n d"],
@@ -23,7 +29,6 @@ def squared_distance(
 
     dist = jax.vmap(jax.vmap(dist, (None, 0)), (0, None))
     return dist(x1, x2)
-
 
 def kernel(
     x1: Float[Array, "n d"],
@@ -74,10 +79,16 @@ def admm_x_update_loss(
     x_train: Float[Array, "n d"],
     y_train: Float[Array, "n"],
 ):
-    theta, g = admm_x[:-1], admm_x[-1]
+    #theta, g = admm_x[:-1], admm_x[-1]
+    theta, _ = admm_x[:-1], admm_x[-1]
+    #g = 0.1
+    #g = 0.01
+    g = 0.001
+    print("ignoring g arg.")
     loglik, _, _ = likelihood(theta, g, x_train, y_train)
     lagrangian = 0.5 * rho * jnp.sum((admm_x - admm_z + admm_u) ** 2)
     return -loglik + lagrangian
+
 
 
 def admm_x_update(
@@ -89,18 +100,27 @@ def admm_x_update(
     y_train: Float[Array, "n o"],
     bounds: Float[Array, "2 o d+1"],
 ) -> Float[Array, "o d+1"]:
-    new_x = [
-        scipy.optimize.minimize(
+    new_x = []
+    for i, x, z, u, y, bmin, bmax in zip(range(admm_x.shape[0]), admm_x, admm_z, admm_u, y_train.T, *bounds):
+        #print(i)
+        x_train_i = jnp.copy(x_train)
+        if SELF_ZERO:
+            # Stores indices of columns pertaining to current marker. 
+            cm_dims = jnp.arange(x_train.shape[0]).reshape([-1,3])[i//3,:] 
+            x_train_i = x_train_i.at[:,cm_dims].set(0.)
+
+        ret = scipy.optimize.minimize(
             fun=admm_x_update_loss,
             x0=x,
-            args=(z, u, rho, x_train, y),
+            args=(z, u, rho, x_train_i, y),
             jac=True,
             method="L-BFGS-B",
             bounds=[(a, b) for a, b in zip(bmin, bmax)],
-            options=dict(maxiter=10, ftol=EPS, gtol=0),
+            #options=dict(maxiter=10, ftol=EPS, gtol=0),
+            options=dict(maxiter=1000, ftol=EPS, gtol=0),
         ).x
-        for x, z, u, y, bmin, bmax in zip(admm_x, admm_z, admm_u, y_train.T, *bounds)
-    ]
+        new_x.append(ret)
+    #
     new_x = jnp.stack(new_x, axis=0)
     return new_x
 
@@ -126,25 +146,43 @@ def admm_z_update(
 
 
 def admm(
-    x0: Float[Array, "o d+1"],
     x_train: Float[Array, "n d"],
     y_train: Float[Array, "n o"],
     bounds: Float[Array, "2 o d+1"],
     l1_penalty: float,
-    max_iterations: int,
-    tollerance: float,
+    x0: Float[Array, "o d+1"] | None = None,
+    z0: Float[Array, "o d+1"] | None = None,
+    u0: Float[Array, "o d+1"] | None = None,
+    rho : float | None = None,
+    max_iterations: int = 100,
+    tollerance: float = 1e-4,
 ):
-    admm_x = x0
-    admm_z = x0
-    admm_u = jnp.zeros_like(x0)
-    rho = 1.0
+    if x0 is None:
+        admm_x = jnp.zeros([y_train.shape[1], x_train.shape[1]+1])
+    else:
+        admm_x = x0
+    if z0 is None:
+        admm_z = x0
+    else:
+        admm_z = z0
+    if u0 is None:
+        admm_u = jnp.zeros_like(x0)
+    else:
+        admm_u = u0
+    #rho = 1.0
+    #print("rho is diff!")
+    #rho = 1e5
+    print("rho is lam!")
+    rho = l1_penalty
 
     trajectory = [(admm_x, admm_z, admm_u, rho)]
     for iter in (pbar := tqdm(range(max_iterations), desc="ADMM")):
         new_admm_x = admm_x_update(
             admm_x, admm_z, admm_u, rho, x_train, y_train, bounds
         )
+        #print(f"New x: {admm_x}")
         new_admm_z = admm_z_update(new_admm_x, admm_z, admm_u, rho, l1_penalty, bounds)
+        #print(f"New z: {admm_z}")
         new_admm_u = admm_u + new_admm_x - new_admm_z
 
         # check convergence
@@ -154,20 +192,23 @@ def admm(
         dual_residual = rho * norm(new_admm_z - admm_z, axis=-1)
         dual_target = rho * norm(admm_u, axis=-1)
         dual_ok = dual_residual < EPS + tollerance * dual_target
-        pbar.set_postfix(
-            {
-                "primal:": (primal_residual / (primal_target + EPS)).max(),
-                "dual:": (dual_residual / (dual_target + EPS)).max(),
-            }
-        )
 
         # update rho to balance primal and dual residuals
-        if primal_residual.sum() > 10 * dual_residual.sum():
-            rho = 2 * rho
-            new_admm_u = new_admm_u / 2
-        elif dual_residual.sum() > 10 * primal_residual.sum():
-            rho = rho / 2
-            new_admm_u = new_admm_u * 2
+        if ADAPT_RHO:
+            if primal_residual.sum() > 10 * dual_residual.sum():
+                rho = 2 * rho
+                new_admm_u = new_admm_u / 2
+            elif dual_residual.sum() > 10 * primal_residual.sum():
+                rho = rho / 2
+                new_admm_u = new_admm_u * 2
+
+        pbar.set_postfix(
+            {
+                "primal:": f"{(primal_residual / (primal_target + EPS)).max():.5f}",
+                "dual:": f"{(dual_residual / (dual_target + EPS)).max():.5f}",
+                "rho": rho,
+            }
+        )
 
         # update state and possibly early stop
         admm_x, admm_z, admm_u = new_admm_x, new_admm_z, new_admm_u
@@ -209,7 +250,6 @@ class GaussianProcessRegressor:
     l1_penalty: float
     max_iterations: int = 1000
     tollerance: float = 1e-4
-    multi_start: int = 1
     seed: int = 42
     verbose: bool = False
     init_theta: Optional[Float[Array, "o d"]] = None
@@ -222,7 +262,7 @@ class GaussianProcessRegressor:
     trajectory: list = field(init=False)
     bounds: Float[Array, "2 o d"] = field(init=False)
 
-    def fit(self, x: Float[Array, "n d"], y: Float[Array, "n 1"]):
+    def fit(self, x: Float[Array, "n d"], y: Float[Array, "n 1"], method ='admm'):
         n, d = x.shape
         n, o = y.shape
 
@@ -259,44 +299,145 @@ class GaussianProcessRegressor:
             print()
 
         results = []
-        for rng_key in jr.split(jr.key(self.seed), self.multi_start):
-            # get a random init point
-            rng_a, rng_b = jr.split(rng_key)
-            a = jr.uniform(rng_a)
-            init_theta = a * init_theta + (1 - a) * jr.uniform(
-                rng_b,
-                init_theta.shape,
-                minval=bounds[0, ..., :-1],
-                maxval=bounds[1, ..., :-1],
-            )
-            x0 = jnp.concatenate([init_theta, init_g[:, None]], axis=-1)
+        x0 = jnp.concatenate([init_theta, init_g[:, None]], axis=-1)
 
-            # run admm
+        # run admm
+        if method=='admm':
             trajectory = admm(
-                x0=x0,
                 x_train=x,
                 y_train=y,
                 bounds=self.bounds,
                 l1_penalty=self.l1_penalty,
+                x0=x0,
                 max_iterations=self.max_iterations,
                 tollerance=self.tollerance,
             )
+        elif method=='pgd':
+            import IPython; IPython.embed()
 
-            # extract the optimal parameters and infer the rest
-            admm_x, admm_z, admm_u, rho = trajectory[-1]
-            theta = admm_x[:, :-1]
-            g = admm_x[:, -1]
-            llk, b, nu = jax.vmap(likelihood, in_axes=(0, 0, None, 1))(theta, g, x, y)
-            glasso = jnp.sum(norm(rearrange(theta, "o (d k) -> (o k) d", k=3), axis=0))
-            loss = -llk.sum() + self.l1_penalty * glasso
-            results.append((loss, trajectory, theta, g, b, nu))
-            if self.verbose:
-                print(f"Optimal theta: {theta}")
-                print(f"Optimal g: {g}")
-                print(f"Optimal b: {b}")
-                print(f"Optimal nu: {nu}")
-                print(f"Log-likelihood at optimum: {llk}")
-                print()
+            def prox_l1(theta, lam):
+                theta = rearrange(theta, "o (d k) -> (o k) d", k=3)
+                prox = jnp.maximum(0, 1 - (lam / norm(theta, axis=0)))
+                theta = prox * theta
+                theta = rearrange(theta, "(o k) d -> o (d k)", k=3)
+                return theta
+
+            x_use = jnp.stack([x for _ in range(x.shape[1]) ])
+            if SELF_ZERO:
+                for i in range(x.shape[1]):
+                    cm_dims = jnp.arange(x.shape[0]).reshape([-1,3])[i//3,:] 
+                    x_use = x_use.at[i,:,cm_dims].set(0.)
+            
+            def _total_nll(theta, g, x_use, y):
+                print("assuming that we are not deleting self!")
+                #likelihood(theta[1,:], g[1,:], x, y[:,1])
+                lls, _, _ = jax.vmap(likelihood, [0,0,0,1])(theta, g, x_use, y)
+                return(-jnp.sum(lls))
+            total_nll = jax.jit(jax.value_and_grad(_total_nll, argnums=(0,1)))
+
+            theta = init_theta
+            g = init_g[:,None]
+            #g = jnp.ones_like(init_g[:,None])
+
+            #lr_theta = 1e-3
+            lr_theta = 1e-4
+            iters = 1000    
+
+            learn_g = False
+            #learn_g = True
+            chill_g = True
+
+            #g_cands = jnp.logspace(jnp.log10(g_bounds[0]), jnp.log10(g_bounds[1]),num=10)
+
+            traj_theta = jnp.nan*jnp.zeros((iters,)+theta.shape)
+            traj_g = jnp.nan*jnp.zeros((iters,)+g.shape)
+            costs = jnp.nan*jnp.zeros(iters)
+            costs_f = jnp.nan*jnp.zeros(iters)
+            costs_g = jnp.nan*jnp.zeros(iters)
+            for i in tqdm(range(iters)):
+
+                if i == 658:
+                    break
+
+                # Assuming common g for all problems! 
+                if learn_g:
+                    if chill_g:
+                        lb = jnp.power(10, (i/iters) * -4 + (1-i/iters)*-1)
+                        g_cands = jnp.logspace(lb, jnp.log10(g_bounds[1]),num=10)
+                    else:
+                        g_cands = jnp.logspace(jnp.log10(g_bounds[0]), jnp.log10(g_bounds[1]),num=10)
+                    g_grid = jnp.stack([g_cands for _ in range(g.shape[0])],axis=1)[:,:,None]
+
+                    g_cost, _ = jax.vmap(total_nll, [None,0,None,None])(theta, g_grid, x_use, y)
+                    g_opt = g_cands[jnp.argmin(g_cost)]
+                    g = jnp.ones_like(g)*g_opt
+
+                # GD step.
+                cost, (theta_grad, g_grad) = total_nll(theta, g, x_use, y)
+                theta = theta - lr_theta*theta_grad
+                theta = prox_l1(theta, self.l1_penalty * lr_theta)
+                #g = g - lr_g*g_grad
+
+                # Projection
+                print("this should be before prox")
+                theta = theta.clip(*bounds[:,:,:-1])
+                #g = g.clip(*bounds[:,:,-1:])
+
+                pen = self.l1_penalty*jnp.sum(jnp.sqrt(jnp.sum(jnp.square(theta),axis=0)))
+                costs_f = costs_f.at[i].set(cost)
+                costs_g = costs_g.at[i].set(pen)
+                costs = costs.at[i].set(cost+pen)
+                traj_theta = traj_theta.at[i,:,:].set(theta)
+                traj_g = traj_g.at[i].set(g)
+
+                if i > 0:
+                    d = costs[i] - costs[i-1]
+                    if d > 0:
+                        print(i)
+                            
+
+            fig = plt.figure(figsize=[15,5])
+            plt.subplot(1,3,1)
+            plt.plot(costs, label = 'f+g')
+            plt.plot(costs_f, label = 'nll')
+            plt.plot(costs_g, label = 'pen')
+            plt.legend()
+            ax = plt.gca().twinx()
+            ax.scatter(jnp.arange(iters-1),jnp.diff(costs))
+            plt.axhline(0, alpha=0.5, color='gray')
+            plt.title('Cost')
+            plt.subplot(1,3,2)
+            for i in range(theta.shape[0]):
+                for j in range(theta.shape[0]):
+                    plt.plot(traj_theta[:,i,j])
+            plt.title(r"$\theta$")
+            plt.subplot(1,3,3)
+            plt.plot(traj_g[:,:,0])
+            plt.title("g")
+            plt.savefig('pgd.pdf')
+            plt.close()
+
+
+        else:
+            raise Exception('Unknown method in fit.')
+
+        # extract the optimal parameters and infer the rest
+        admm_x, admm_z, admm_u, rho = trajectory[-1]
+        theta = admm_x[:, :-1]
+        g = admm_x[:, -1]
+        llk, b, nu = jax.vmap(likelihood, in_axes=(0, 0, None, 1))(theta, g, x, y)
+        glasso = jnp.sum(norm(rearrange(theta, "o (d k) -> (o k) d", k=3), axis=0))
+        loss = -llk.sum() + self.l1_penalty * glasso
+        results.append((loss, trajectory, theta, g, b, nu))
+        if self.verbose:
+            print(f"Optimal theta: {theta}")
+            print(f"Optimal g: {g}")
+            print(f"Optimal b: {b}")
+            print(f"Optimal nu: {nu}")
+            print(f"Log-likelihood at optimum: {llk}")
+            print()
+
+        #import IPython; IPython.embed()
 
         # best result (minimum loss) across the multiple random restarts
         trajectory, theta, g, b, nu = min(results, key=lambda x: x[0])[1:]
---
+++ metest.py
@@ -0,0 +1,147 @@
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+import numpy as np
+
+from sklearn.metrics import r2_score
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import gp
+import os
+import pandas as pd
+from einops import rearrange
+
+import matplotlib.pyplot as plt
+
+from time import time
+
+os.makedirs("test4", exist_ok=True)
+jax.config.update("jax_enable_x64", True)
+
+st = time()
+
+############################################################
+# Parameters
+############################################################
+#realdata = False
+realdata = True
+#warmstart = False
+warmstart = True
+reverse = False
+#reverse = True
+#markers = 2
+markers = 3
+#markers = 4
+#markers = 13
+subsample_factor = 100
+
+normalize = True
+detrend = False
+
+I = 10
+#I = 2
+#admm_iters = 100
+admm_iters = 500
+
+if realdata:
+    #lambdas = 10 ** jnp.linspace(3, 3.5, I)
+    #lambdas = 10 ** jnp.linspace(2, 4, I)
+    lambdas = 10 ** jnp.linspace(0, 4, I)
+    #lambdas = 10 ** jnp.linspace(2, 5, I)
+else:
+    lambdas = 10 ** jnp.linspace(0, 5, I)
+
+############################################################
+# Variants
+############################################################
+
+if realdata:
+    path = "OneDrive_1_03-02-2026/Left_bf_10kmh_Filtered.txt"
+    df = pd.read_csv(path, sep="\t", skiprows=[0, 2, 3, 4], index_col=1)
+    df = df.drop(columns=["Unnamed: 0"])
+    df.reset_index(inplace=True)
+    print(df.shape)
+    x = df.iloc[:, 1:].values
+    x = x[~np.isnan(x).any(axis=1)]
+    # subsample and make a mock regression problem
+    x = jnp.array(x[::subsample_factor, : markers * 3])
+    x_train, x_test = x[::2, :], x[1::2, :]
+    y_train, y_test = x[::2, :], x[1::2, :]
+    print("train:", x_train.shape, y_train.shape)
+    print("test:", x_test.shape, y_test.shape)
+    n, d = x_train.shape
+    n, o = y_train.shape
+else:
+    gg = np.linspace(0,1,num=100)
+    gg2 = np.square(gg)
+    assert M==2
+    x_train = np.stack([gg,gg,gg,np.sin(2*np.pi*gg),np.cos(2*np.pi*gg),np.sin(4*np.pi*gg)], axis = 1)
+    y_train = y_test = x_test = np.copy(x_train)
+
+M = x_train.shape[1]//3
+
+#if detrend:
+#    print("detrending!")
+#    for i in range(y_train.shape[1]):
+#        #np.linalg.ols
+
+if normalize:
+    y_test = (y_test - jnp.mean(y_train,axis=0)[None,:]) / jnp.std(y_train,axis=0)[None,:]
+    y_train = (y_train - jnp.mean(y_train,axis=0)[None,:]) / jnp.std(y_train,axis=0)[None,:]
+
+    x_test = (x_test - jnp.min(x_train,axis=0)[None,:]) / (jnp.max(x_train,axis=0)-jnp.min(x_train,axis=0))[None,:]
+    x_train = (x_train - jnp.min(x_train,axis=0)[None,:]) / (jnp.max(x_train,axis=0)-jnp.min(x_train,axis=0))[None,:]
+
+print("Train points shape:", x_train.shape)  # (N_TRAIN, 2)
+print("Test points shape:", x_test.shape)  # (N_GRID*N_GRID, 2)
+print()
+
+############################################################
+# Single run fun.
+############################################################
+model = gp.GaussianProcessRegressor(
+    l1_penalty=1e2,
+    max_iterations=100,
+    tollerance=1e-4,
+    verbose=True,
+    #init_theta=np.zeros([x_train.shape[1], x_train.shape[1]])
+)
+_ = model.fit(x=x_train, y=y_train)
+
+############################################################
+# Run model at high penalty to extract single sensor.
+############################################################
+
+if reverse:
+    lambdas = lambdas[::-1]
+
+thetas = np.nan*np.zeros([I,x_train.shape[1], x_train.shape[1]])
+gns = np.nan*np.zeros([I,M])
+for li,l in enumerate(lambdas):
+    reinit = model.parameters.theta if warmstart and li > 0 else None
+    model = gp.GaussianProcessRegressor(
+        l1_penalty=l,
+        max_iterations=admm_iters,
+        tollerance=1e-4,
+        verbose=True,
+        init_theta=reinit,
+        #init_theta=np.zeros([x_train.shape[1], x_train.shape[1]])
+    )
+    _ = model.fit(x=x_train, y=y_train)
+    thetas[li,:,:] = model.parameters.theta 
+    gns[li,:] = jnp.sum(jnp.square(rearrange(thetas[li,:,:], "o (d k) -> (o k) d", k=3)), axis = 0)
+
+fig = plt.figure()
+plt.subplot(1,2,1)
+plt.plot(lambdas, jnp.round(gns, 8))
+plt.xscale('log')
+plt.yscale('log')
+plt.subplot(1,2,2)
+for i in range(thetas.shape[1]):
+    plt.plot(lambdas, jnp.round(thetas[:,i],8))
+plt.xscale('log')
+plt.savefig("temp.pdf")
+plt.close()
+
+print("Diff time:")
+print(time() - st)
